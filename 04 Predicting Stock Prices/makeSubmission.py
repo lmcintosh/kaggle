1dd6bda6f72509c963a9f6399724b96e5d55734a
Lane McIntosh <lanemcintosh@gmail.com>
2013-09-11 14:22:14 -0700

    working code for 3rd degree polynomial function of last observed value

--- 04 Predicting Stock Prices/makeSubmission.py
+++ 04 Predicting Stock Prices/makeSubmission.py
@@ -31,17 +31,43 @@ def main():
 
 
     #DATA EXPLORATION
-    #Exponential moving average at a single desired discountRate
-    discountRate = [0.9]
-    pred = np.zeros((len(trainingDays),sum(isOutput)))
-    for day in range(0,len(trainingDays)):
+    iterations   = 100
+    learningRate = 0.000001
+    h            = (2e-15)**(1./3)
+    coeff        = [0,1,0]
+    error        = []
+    gradient     = [0,0,0]
+    for i in range(0,iterations):
+        pred0P = np.zeros((len(trainingDays),sum(isOutput)))
+        pred0M = np.zeros((len(trainingDays),sum(isOutput)))
+        pred1P = np.zeros((len(trainingDays),sum(isOutput)))
+        pred1M = np.zeros((len(trainingDays),sum(isOutput)))
+        pred2P = np.zeros((len(trainingDays),sum(isOutput)))
+        pred2M = np.zeros((len(trainingDays),sum(isOutput)))
+        #pred3P = np.zeros((len(trainingDays),sum(isOutput)))
+        #pred3M = np.zeros((len(trainingDays),sum(isOutput)))
+        pred   = np.zeros((len(trainingDays),sum(isOutput)))
+        for day in range(0,len(trainingDays)):
             for stock in range(0,sum(isOutput)):
-                for j in range(0,numRows):
-                    pred[day,stock] = discountRate[-1]*trainOutput[day,j,stock] + (1-discountRate[-1])*pred[day,stock]
-    
-    error = err.maeFun(target,pred[0:200,:])
-    print "Results: " + str(error) + " Discount Rate: " + str(discountRate[-1])
+                pred0P[day,stock] = (coeff[2])*(trainOutput[day,-1,stock])**2 + (coeff[1])*trainOutput[day,-1,stock] + (coeff[0] + h)
+                pred0M[day,stock] = (coeff[2])*(trainOutput[day,-1,stock])**2 + (coeff[1])*trainOutput[day,-1,stock] + (coeff[0] - h)
+                pred1P[day,stock] = (coeff[2])*(trainOutput[day,-1,stock])**2 + (coeff[1]+h)*trainOutput[day,-1,stock] + (coeff[0])
+                pred1M[day,stock] = (coeff[2])*(trainOutput[day,-1,stock])**2 + (coeff[1]-h)*trainOutput[day,-1,stock] + (coeff[0])
+                pred2P[day,stock] = (coeff[2]+h)*(trainOutput[day,-1,stock])**2 + (coeff[1])*trainOutput[day,-1,stock] + (coeff[0])
+                pred2M[day,stock] = (coeff[2]-h)*(trainOutput[day,-1,stock])**2 + (coeff[1])*trainOutput[day,-1,stock] + (coeff[0])
+                #pred3P[day,stock] = (coeff[3]+h)*(trainOutput[day,-1,stock])**3 + (coeff[2])*(trainOutput[day,-1,stock])**2 + (coeff[1])*trainOutput[day,-1,stock] + (coeff[0])
+                #pred3M[day,stock] = (coeff[3]-h)*(trainOutput[day,-1,stock])**3 + (coeff[2])*(trainOutput[day,-1,stock])**2 + (coeff[1])*trainOutput[day,-1,stock] + (coeff[0])
+                pred[day,stock]   = (coeff[2])*(trainOutput[day,-1,stock])**2 + (coeff[1])*trainOutput[day,-1,stock] + (coeff[0])
 
+        gradient[0] = (err.maeFun(target,pred0P[0:200,:]) - err.maeFun(target,pred0M[0:200,:]))/(2*h)
+        gradient[1] = (err.maeFun(target,pred1P[0:200,:]) - err.maeFun(target,pred1M[0:200,:]))/(2*h)
+        gradient[2] = (err.maeFun(target,pred2P[0:200,:]) - err.maeFun(target,pred2M[0:200,:]))/(2*h)
+        #gradient[3] = (err.maeFun(target,pred3P[0:200,:]) - err.maeFun(target,pred3M[0:200,:]))/(2*h)
+       
+        coeff = [coeff[x] - learningRate*gradient[x] for x in range(0,len(gradient))]
+
+        error.append(err.maeFun(target,pred[0:200,:])) #maeFun(actual,pred)
+        print "Results: " + str(error[-1]) + " Coefficients: " + str(coeff)
 
     #generate predictions and save to file
     np.savetxt('Data/submission'+str(datetime.date.today())+'.csv', pred[200:510,:], delimiter=',', fmt='%f')  #predictions for file 201 to 510
